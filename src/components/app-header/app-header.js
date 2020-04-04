78e75c2b02334efe4a154989252ed595e6f2d05f
Vladislav Gavrish <vdgavrish@gmail.com>
2020-04-04 15:07:18 +0300

    added component folder

---
+++ src/components/app-header/app-header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AppHeader = () => {
+  return(
+    <div>
+      <h1>Vlad Petrov</h1>
+      <h2>5 записей, из них понравились 0</h2>
+    </div>
+  )
+}
+
+export default AppHeader;
---
+++ src/components/app/app.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import AppHeader from '../app-header/app-header'
+
+const App = () => {
+  return(
+    <div>
+      <AppHeader />
+    </div>
+  )
+}
+
+export default App;
