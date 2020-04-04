d4083aada5e9bc92d7e9318091662c7c03b7c4f3
Vladislav Gavrish <vdgavrish@gmail.com>
2020-04-04 16:41:22 +0300

    added css styles

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -5,6 +5,14 @@ import PostStatusFilter from '../post-status-filter/post-status-filter';
 import PostList from '../post-list/post-list'
 import PostAddForm from '../post-add-form/post-add-form'
 
+import './app.css'
+import '../app-header/app-header.css'
+import '../search-panel/search-panel.css'
+import '../post-list/post-list.css'
+import '../post-list-item/post-list-item.css'
+import '../post-add-form/post-add-form.css'
+
+
 const App = () => {
   return(
     <div className = "app">
