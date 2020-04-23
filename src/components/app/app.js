79f524aa86b70ca76905a570e272050b71f9261e
Vladislav Gavrish <vdgavrish@gmail.com>
2020-04-23 09:25:59 +0300

    made filter work

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -22,13 +22,15 @@ export default class App extends React.Component {
         {label: "That is so fun!", important: false, like: false, id: 2},
         {label: "I need a break...", important: true, like: true, id: 3}
       ],
-      term: ''
+      term: '',
+      filter: 'all'
     };
     this.deleteItem = this.deleteItem.bind(this);
     this.addItem = this.addItem.bind(this);
     this.onToggleImportant = this.onToggleImportant.bind(this);
     this.onToggleLiked = this.onToggleLiked.bind(this);
     this.onUpdateSearch = this.onUpdateSearch.bind(this);
+    this.onFilterSelect = this.onFilterSelect.bind(this);
 
     this.maxId = 4;
   }
@@ -112,13 +114,25 @@ export default class App extends React.Component {
     this.setState({term});
   }
 
+  filterPost(items, filter) {
+    if(filter === 'like') {
+      return items.filter(item => item.like);
+    } else {
+      return items;
+    }
+  }
+
+  onFilterSelect(filter) {
+    this.setState({filter});
+  }
+
   render() {
-    const {data, term} = this.state;
+    const {data, term, filter} = this.state;
 
     const liked = data.filter(item => item.like).length;
     const allPosts = data.length;
 
-    const visiblePosts = this.searchPost(data, term);
+    const visiblePosts = this.filterPost(this.searchPost(data, term), filter);
 
     return(
       <div className = {AppStyles.app}>
@@ -128,7 +142,10 @@ export default class App extends React.Component {
         />
         <div className = "search-panel d-flex">
           <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
-          <PostStatusFilter />
+          <PostStatusFilter 
+            filter={filter}
+            onFilterSelect={this.onFilterSelect}
+          />
         </div>
         <PostList posts = {visiblePosts}
                   onDelete={this.deleteItem}
